38ea0ebab3b263c60533c85aba4b10df0aa1fc08
saru1439s7 <d.n.saru39@gmail.com>
2021-10-05 13:04:10 +0000

    learn inject

---
+++ p4.rb
@@ -0,0 +1,34 @@
+# numbers=[1,2,3,4]
+# sum=0
+# numbers.each do|n|
+#  sum += n
+# end
+
+# puts sum
+
+# a=[1,2,3,1,2,3,4]
+# a.delete(2)
+ 
+#  puts a
+ 
+#  a=[1,2,3,1,2,3]
+#  a.delete_if do |n|
+#    n.odd?
+#  end
+#  puts a
+ 
+#  p('ここ4.3.4')
+ 
+#  numbers=[1,2,3,4]
+#  sum=0
+#  numbers.each do |n|
+#    sum +=n
+#  end
+ 
+#  puts sum
+ 
+#  p"4.4.1"
+#  members=[1,2,3,4,5,6]
+#  new_members=[]
+#  members.each{|n|new_members<<n*10}
+# puts new_members.each{|n|new_members<<n*10}
