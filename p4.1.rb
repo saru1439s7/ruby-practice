bd51c441742ba77b02d53110a0bfdd2b120aa6ba
saru1439s7 <d.n.saru39@gmail.com>
2021-10-06 10:44:43 +0000

    RGB　program

--- p4.1.rb
+++ p4.1.rb
@@ -8,25 +8,25 @@
 
 # a=[1,2,3,1,2,3,4]
 # a.delete(2)
- 
+
 #  puts a
- 
+
 #  a=[1,2,3,1,2,3]
 #  a.delete_if do |n|
 #    n.odd?
 #  end
 #  puts a
- 
+
 #  p('ここ4.3.4')
- 
+
 #  numbers=[1,2,3,4]
 #  sum=0
 #  numbers.each do |n|
 #    sum +=n
 #  end
- 
+
 #  puts sum
- 
+
 #  p"4.4.1"
 #  members=[1,2,3,4,5,6]
 #  new_members=[]
@@ -43,3 +43,29 @@ puts even_numbers
 numbers = [1,2,3,4,5,6]
 sum=numbers.inject(0){|result,n| result+n}
 puts sum
+
+def charge(age)
+ case age
+ when 0..5
+   0
+ when 6..12
+   300
+ when 13..18
+   600
+  else
+   1000
+ end  
+end
+print charge(30)
+
+#4.5.5
+numbers = (1..4).to_a
+ sum = 0
+ numbers.each {|n| sum+= n}
+ print sum
+ 
+numbers=[]
+(1..20).step(2){ |n| numbers<< n }
+print numbers
+ p "次4.6"
+ 
